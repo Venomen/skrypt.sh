854ee3f0ee223668e97089d2a67f83623da5819b
Dawid Deręgowski <Venomen@users.noreply.github.com>
2019-05-17 01:24:09 +0200

    First commit

---
+++ apt-new-server.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+apt-get install iotop iftop htop gzip mc figlet vim zsh curl wget lynx screen ccze rsync bzip2 mutt netcat nethogs openssl sudo sed tcpdump traceroute unzip zip exuberant-ctags -y;
+wget download.rz1.pl/.zshrc
+wget download.rz1.pl/.vimrc
+wget download.rz1.pl/.vim.tar
