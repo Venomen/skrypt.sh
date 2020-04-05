90d08cacdcd32cae73c0657e1f548af8463c56c4
Dawid Deręgowski <Venomen@users.noreply.github.com>
2020-04-05 23:40:45 +0200

    few changes

--- apt-new-server.sh
+++ apt-new-server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get install iotop iftop htop gzip mc figlet vim zsh curl wget lynx screen ccze rsync bzip2 mutt netcat nethogs openssl sudo sed tcpdump traceroute unzip zip exuberant-ctags -y;
+apt-get install telnet iotop iftop htop gzip mc figlet vim zsh curl wget lynx screen ccze rsync bzip2 mutt netcat nethogs openssl sudo sed tcpdump traceroute unzip zip exuberant-ctags multitail -y;
 wget download.rz1.pl/.zshrc
 wget download.rz1.pl/.vimrc
 wget download.rz1.pl/.vim.tar
